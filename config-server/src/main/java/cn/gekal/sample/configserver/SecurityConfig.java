05771b45c5f8bdb173e287808a95221c68479b92
gekal <liu.hongying@hotmail.com>
2021-03-21 19:51:13 +0900

    init commit

--- config-server/src/main/java/cn/gekal/sample/configserver/SecurityConfig.java
+++ config-server/src/main/java/cn/gekal/sample/configserver/SecurityConfig.java
@@ -1,4 +1,29 @@
 package cn.gekal.sample.configserver;
 
-public class WebSecurityConfigurerAdapter {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .passwordEncoder(new BCryptPasswordEncoder())
+                .withUser("client")
+                .password("password")
+                .roles("CLIENT");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic();
+        http.authorizeRequests()
+                .anyRequest()
+                .authenticated();
+    }
 }
